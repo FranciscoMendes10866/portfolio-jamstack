1225dd472b6406f8e6b31111f1b7ca9256993471
Francisco Mendes <franciscomendes@Franciscos-MBP.lan>
2020-07-08 17:13:25 +0100

    netlify forms

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -4,36 +4,38 @@
       <div class="container mx-auto">
         <div class="grid grid-cols-2">
           <div>
-            <h1 class="text-4xl font-bold tracking-wide">
-              Let's talk.
-            </h1>
+            <h1 class="text-4xl font-bold tracking-wide">Let's talk.</h1>
           </div>
           <div>
-            <form class="w-full max-w-lg">
+            <form class="w-full max-w-lg" name="contact" method="POST" data-netlify="true">
               <div class="flex flex-wrap mb-6">
                 <div class="w-full px-3">
                   <input
                     class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                     type="text"
-                    placeholder="Name"
-                  >
+                    name="name"
+                    placeholder="Type your name"
+                  />
                 </div>
                 <div class="w-full px-3 mt-4">
                   <input
                     class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                     type="email"
-                    placeholder="Email"
-                  >
+                    name="email"
+                    placeholder="Type your email"
+                  />
                 </div>
                 <div class="w-full px-3 mt-4">
                   <textarea
                     class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                     type="text"
-                    placeholder="Message"
+                    name="message"
+                    placeholder="Say Hi!"
                   />
                 </div>
                 <div class="w-full px-3 mt-4">
                   <button
+                    type="submit"
                     class="px-4 py-2 bg-gray-700 hover:bg-gray-400 rounded text-white hover:text-black"
                   >
                     Send&nbsp;&nbsp;
