f1985872551cad34c92d16077516f8f5b8cdc626
Francisco Mendes <franciscomendes@Franciscos-MBP.lan>
2020-08-01 03:02:00 +0100

    added kursor.js library and AOS

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
     <section class="bg-white py-40 md:py-64">
       <div class="container mx-auto">
         <div class="grid grid-cols-1">
-          <div>
+          <div data-aos="fade-up" data-aos-duration="500">
             <h1
               class="ml-4 md:ml-0 text-left md:text-center text-2xl md:text-4xl tracking-wide md:leading-loose"
             >
@@ -21,21 +21,25 @@
       <div class="container mx-auto">
         <div class="grid grid-cols-1 md:grid-cols-2">
           <div>
-            <h1 class="ml-4 md:ml-0 text-2xl md:text-4xl font-bold tracking-wide">Front-end</h1>
+            <h1
+              class="ml-4 md:ml-0 text-2xl md:text-4xl font-bold tracking-wide"
+              data-aos="fade-right"
+            >Front-end</h1>
           </div>
           <div>
             <p
               class="ml-4 md:ml-0 text-md md:text-lg font-normal tracking-wide leading-loose mt-4 md:mt-0"
+              data-aos="fade-left"
             >I like to code things from scratch, and enjoy bringing ideas to life in the browser.</p>
             <div
               class="grid grid-cols-2 mt-6 text-gray-600 ml-4 md:ml-0 text-md md:text-lg font-normal tracking-wide"
             >
-              <div>
+              <div data-aos="fade-left" data-aos-duration="500">
                 <h1>
                   <ion-icon name="logo-css3" />&nbsp;&nbsp;Tailwind CSS
                 </h1>
               </div>
-              <div>
+              <div data-aos="fade-left" data-aos-duration="600">
                 <h1>
                   <ion-icon name="logo-css3" />&nbsp;&nbsp;Bulma CSS
                 </h1>
@@ -44,12 +48,12 @@
             <div
               class="grid grid-cols-2 mt-6 text-gray-600 ml-4 md:ml-0 text-md md:text-lg font-normal tracking-wide"
             >
-              <div>
+              <div data-aos="fade-left" data-aos-duration="700">
                 <h1>
                   <ion-icon name="logo-css3" />&nbsp;&nbsp;Vuetify
                 </h1>
               </div>
-              <div>
+              <div data-aos="fade-left" data-aos-duration="800">
                 <h1>
                   <ion-icon name="logo-css3" />&nbsp;&nbsp;Vuesax
                 </h1>
@@ -64,21 +68,25 @@
       <div class="container mx-auto">
         <div class="grid grid-cols-1 md:grid-cols-2">
           <div>
-            <h1 class="ml-4 md:ml-0 text-2xl md:text-4xl font-bold tracking-wide">Back-end</h1>
+            <h1
+              class="ml-4 md:ml-0 text-2xl md:text-4xl font-bold tracking-wide"
+              data-aos="fade-right"
+            >Back-end</h1>
           </div>
           <div>
             <p
               class="ml-4 md:ml-0 text-md md:text-lg font-normal tracking-wide leading-loose mt-4 md:mt-0"
+              data-aos="fade-left"
             >I love the world of Node.js and since the very first API that a built, I haven't stopped coding.</p>
             <div
               class="grid grid-cols-2 mt-6 text-gray-600 ml-4 md:ml-0 text-md md:text-lg font-normal tracking-wide"
             >
-              <div>
+              <div data-aos="fade-left" data-aos-duration="500">
                 <h1>
                   <ion-icon name="logo-nodejs" />&nbsp;&nbsp;Node.js
                 </h1>
               </div>
-              <div>
+              <div data-aos="fade-left" data-aos-duration="600">
                 <h1>
                   <ion-icon name="logo-nodejs" />&nbsp;&nbsp;Express.js
                 </h1>
@@ -87,12 +95,12 @@
             <div
               class="grid grid-cols-2 mt-6 text-gray-600 ml-4 md:ml-0 text-md md:text-lg font-normal tracking-wide"
             >
-              <div>
+              <div data-aos="fade-left" data-aos-duration="700">
                 <h1>
                   <ion-icon name="logo-nodejs" />&nbsp;&nbsp;Sequelize ORM
                 </h1>
               </div>
-              <div>
+              <div data-aos="fade-left" data-aos-duration="800">
                 <h1>
                   <ion-icon name="logo-nodejs" />&nbsp;&nbsp;Prisma
                 </h1>
@@ -106,7 +114,7 @@
     <section class="bg-white py-48">
       <div class="container mx-auto">
         <div class="grid grid-cols-1">
-          <div>
+          <div data-aos="fade-up" data-aos-duration="500">
             <h1
               class="ml-4 md:ml-0 text-left md:text-center text-2xl md:text-4xl tracking-wide md:leading-loose"
             >
