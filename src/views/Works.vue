6acdc8c1a643317854526e973f9fc67896e638d6
Francisco Mendes <franciscomendes@Franciscos-MBP.lan>
2020-08-11 18:45:34 +0100

    added some more animations

--- src/views/Works.vue
+++ src/views/Works.vue
@@ -30,16 +30,26 @@
       <div class="container mx-auto">
         <div class="grid grid-cols-1 md:grid-cols-2">
           <div>
-            <h1 class="ml-4 md:ml-0 text-2xl md:text-4xl font-bold tracking-wide">Work</h1>
+            <h1
+              data-aos="fade-up"
+              data-aos-duration="500"
+              class="ml-4 md:ml-0 text-2xl md:text-4xl font-bold tracking-wide"
+            >Work</h1>
           </div>
           <div>
             <p
+              data-aos="fade-up"
+              data-aos-duration="600"
               class="mt-4 md:mt-0 ml-4 md:ml-0 text-md md:text-lg font-normal tracking-wide leading-loose"
             >
               Are you interested in working with me on a
               freelance project or hiring me full time?
             </p>
-            <p class="ml-4 md:ml-0 text-md md:text-lg font-normal tracking-wide leading-loose mt-8">
+            <p
+              data-aos="fade-up"
+              data-aos-duration="700"
+              class="ml-4 md:ml-0 text-md md:text-lg font-normal tracking-wide leading-loose mt-8"
+            >
               Feel free to contact me sending an email to:
               <span
                 class="font-bold"
