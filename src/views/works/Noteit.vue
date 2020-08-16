423d43a5b3f901db2e06702dca2b4af1cf7e5707
Francisco Mendes <franciscomendes@Franciscos-MBP.lan>
2020-08-16 14:59:40 +0100

    added noteit project

--- src/views/works/Noteit.vue
+++ src/views/works/Noteit.vue
@@ -1,7 +1,120 @@
 <template>
-  <div>Noteit project</div>
+  <div>
+    <Project :project="project" />
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  components: {
+    Project: () => import('../../components/Project')
+  },
+  data: () => {
+    return {
+      project: {
+        project_name: 'Note it!',
+        tech_stack: [
+          {
+            id: 1,
+            name: 'Vue'
+          },
+          {
+            id: 2,
+            name: 'Vuex'
+          },
+          {
+            id: 3,
+            name: 'Axios'
+          },
+          {
+            id: 4,
+            name: 'Vuesax'
+          },
+          {
+            id: 5,
+            name: 'Adonis'
+          },
+          {
+            id: 6,
+            name: 'PostgreSQL'
+          },
+          {
+            id: 7,
+            name: 'Heroku'
+          },
+          {
+            id: 8,
+            name: 'Vercel'
+          }
+        ],
+        description: {
+          first_paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra neque a eros molestie, a volutpat quam tristique. Morbi ut laoreet eros, quis accumsan ante.',
+          second_paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra neque a eros molestie, a volutpat quam tristique. Morbi ut laoreet eros, quis accumsan ante.'
+        },
+        headers: {
+          first_topic: {
+            title: 'Design.',
+            body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra neque a eros molestie, a volutpat quam tristique. Morbi ut laoreet eros, quis accumsan ante.'
+          },
+          second_topic: {
+            title: 'Implementation.',
+            body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra neque a eros molestie, a volutpat quam tristique. Morbi ut laoreet eros, quis accumsan ante.'
+          },
+          third_topic: {
+            title: 'Challenges.',
+            first_body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra neque a eros molestie, a volutpat quam tristique. Morbi ut laoreet eros, quis accumsan ante.',
+            second_body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra neque a eros molestie, a volutpat quam tristique. Morbi ut laoreet eros, quis accumsan ante.',
+            third_body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra neque a eros molestie, a volutpat quam tristique. Morbi ut laoreet eros, quis accumsan ante.'
+          }
+        },
+        routing: {
+          previous: '/works/docus',
+          next: '/works/locise'
+        },
+        launch_project: {
+          href: 'https://noteit.now.sh/',
+          figma_link: 'https://www.figma.com/',
+          source_code: 'https://github.com/FranciscoMendes10866/Note-it-'
+        },
+        screenshots_design: [
+          {
+            id: 1,
+            src: 'https://res.cloudinary.com/dj5iihhqv/image/upload/v1597419765/nuxt-portfolio/noteit/Screenshot_2020-08-14_Note_it_vn9yys.png',
+            alt: 'Home page'
+          },
+          {
+            id: 2,
+            src: 'https://res.cloudinary.com/dj5iihhqv/image/upload/v1597420889/nuxt-portfolio/noteit/Screenshot_2020-08-14_Note_it_ce20ku.png',
+            alt: 'Sign Up page'
+          },
+          {
+            id: 3,
+            src: 'https://res.cloudinary.com/dj5iihhqv/image/upload/v1597420889/nuxt-portfolio/noteit/Screenshot_2020-08-14_Note_it_1_l8vape.png',
+            alt: 'Sign In page'
+          }
+        ],
+        screenshots_implementation: [
+          {
+            id: 1,
+            src: 'https://res.cloudinary.com/dj5iihhqv/image/upload/v1597420889/nuxt-portfolio/noteit/Screenshot_2020-08-14_Note_it_2_n7bmrh.png',
+            alt: 'Create an account error'
+          },
+          {
+            id: 2,
+            src: 'https://res.cloudinary.com/dj5iihhqv/image/upload/v1597420889/nuxt-portfolio/noteit/Screenshot_2020-08-14_Note_it_3_uin0ex.png',
+            alt: 'Empty dashboard'
+          },
+          {
+            id: 3,
+            src: 'https://res.cloudinary.com/dj5iihhqv/image/upload/v1597420890/nuxt-portfolio/noteit/Screenshot_2020-08-14_Note_it_4_sio2w9.png',
+            alt: 'Project with tasks'
+          }
+        ],
+        bg_img: {
+          first_image: 'background-image: url(https://res.cloudinary.com/dj5iihhqv/image/upload/v1597538742/nuxt-portfolio/noteit/noteit_wo74ed.png)'
+        }
+      }
+    }
+  }
+}
 </script>
