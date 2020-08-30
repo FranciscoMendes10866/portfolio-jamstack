e054fb1a6fe30f38fedd1005121e07df79ee5169
Francisco Mendes <franciscomendes@Franciscos-MBP.lan>
2020-08-30 02:47:55 +0100

    added minary content

--- src/views/works/Minary.vue
+++ src/views/works/Minary.vue
@@ -52,23 +52,22 @@ export default {
           }
         ],
         description: {
-          first_paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra neque a eros molestie, a volutpat quam tristique. Morbi ut laoreet eros, quis accumsan ante.',
-          second_paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra neque a eros molestie, a volutpat quam tristique. Morbi ut laoreet eros, quis accumsan ante.'
+          first_paragraph: 'Minary\'s idea is to create a Blog where people can give some of their knowledge to the world, as if it were Open Knowledge.',
+          second_paragraph: 'People with academic knowledge or simply professional knowledge in the specific area of Design and Tech could write a short article about something that they think is relevant to share and that should be of general knowledge.'
         },
         headers: {
           first_topic: {
             title: 'Design.',
-            body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra neque a eros molestie, a volutpat quam tristique. Morbi ut laoreet eros, quis accumsan ante.'
+            body: 'The design of this application was inspired by the DigitalOcean Blog, I thought the design was amazing so I decided to replicate and implement this project.'
           },
           second_topic: {
             title: 'Implementation.',
-            body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra neque a eros molestie, a volutpat quam tristique. Morbi ut laoreet eros, quis accumsan ante.'
+            body: 'As Docker was used in all projects to create a local database, whether in PostgreSQL or in NoSql. I also decided to do the same with MySQL but the only GUI I like to use is phpmyadmin so the challenge was the creation of a docker-compose in which I defined both images (MySQL and phpmyadmin) as well as creating a network for both if they could communicate.'
           },
           third_topic: {
             title: 'Challenges.',
-            first_body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra neque a eros molestie, a volutpat quam tristique. Morbi ut laoreet eros, quis accumsan ante.',
-            second_body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra neque a eros molestie, a volutpat quam tristique. Morbi ut laoreet eros, quis accumsan ante.',
-            third_body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra neque a eros molestie, a volutpat quam tristique. Morbi ut laoreet eros, quis accumsan ante.'
+            first_body: 'My main challenge was to pass the id parameter through the vue-router so that a selected article could be printed on a given page. That yes, it was difficult for me at the time and without a doubt it was the main challenge.',
+            third_body: 'Another challenge was the use of TypeORM, despite having good documentation it was difficult for me to understand the difference between Active Record and Data Mapper. So far, the only ORM I had used was the Sequelize ORM and so I had only used Active Record. And I found it interesting to learn and read about Data Mapper and the use of repositories.'
           }
         },
         routing: {
