5a13ff7921a38252b6491e5439b262f85345ce9e
Francisco Mendes <franciscomendes@Franciscos-MBP.lan>
2020-09-05 23:04:46 +0100

    added cv to download

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -16,13 +16,25 @@
           </div>
           <div class="px-4 sm:w-1/2 md:w-1/4 mt-4 md:mt-0">
             <h6 class="tracking-wide text-md text-gray-600 font-bold">Pages</h6>
-            <router-link to="/" class="tracking-wide text-sm text-gray-600 no-underline hover:underline">Home</router-link>
+            <router-link
+              to="/"
+              class="tracking-wide text-sm text-gray-600 no-underline hover:underline"
+            >Home</router-link>
             <br />
-            <router-link to="/about" class="tracking-wide text-sm text-gray-600 no-underline hover:underline">About</router-link>
+            <router-link
+              to="/about"
+              class="tracking-wide text-sm text-gray-600 no-underline hover:underline"
+            >About</router-link>
             <br />
-            <router-link to="/works" class="tracking-wide text-sm text-gray-600 no-underline hover:underline">Works</router-link>
+            <router-link
+              to="/works"
+              class="tracking-wide text-sm text-gray-600 no-underline hover:underline"
+            >Works</router-link>
             <br />
-            <router-link to="/contact" class="tracking-wide text-sm text-gray-600 no-underline hover:underline">Contact</router-link>
+            <router-link
+              to="/contact"
+              class="tracking-wide text-sm text-gray-600 no-underline hover:underline"
+            >Contact</router-link>
           </div>
           <div class="px-4 sm:w-1/2 md:w-1/4 mt-4 md:mt-0">
             <h6 class="tracking-wide text-md text-gray-600 font-bold">Works</h6>
@@ -62,12 +74,14 @@
             >Locise</router-link>
           </div>
           <div class="px-4 md:w-1/4 md:ml-auto mt-6 sm:mt-4 md:mt-0">
-            <button
+            <a
+              href="https://drive.google.com/file/d/1-I14ccj4u_u-V_iH46aHmmSM-CN65sB-/view?usp=sharing"
+              target="blank"
               class="transition duration-150 ease-in-out px-4 py-2 border-solid border-2 border-black bg-transparent hover:bg-black text-black hover:text-white"
             >
               Download CV&nbsp;&nbsp;
               <ion-icon name="download-outline" />
-            </button>
+            </a>
           </div>
         </div>
       </div>
